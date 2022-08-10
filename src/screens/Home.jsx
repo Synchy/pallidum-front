37cddbbd0fe18ceb9ba30e7f1f4cc75b80a7b58f
Jérémy <kirijeremy@gmail.com>
2022-08-10 16:51:02 +0200

    create new branch and init svg animation

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,48 @@
 const Home = () => {
   return (
     <div className='Home'>
+      <div className='svgContainer'>
+        <svg
+          width='1398'
+          height='1024'
+          preserveAspectRatio='xMaxYMax meet'
+          viewBox='0 0 1398 1024'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M150.484 120.5C150.484 120.5 202.312 71 207.17 67C212.029 63 214.729 67 214.729 72C214.729 77 211.489 967.5 213.649 968.5C215.808 969.5 1393.8 965 1393.8 968.5C1393.8 972 1393.8 1024 1393.8 1024'
+            stroke='#AA3BFF'
+            strokeWidth='8'
+            strokeLinecap='round'
+          />
+          <path
+            d='M179.637 58L112.693 120'
+            stroke='#CB5CFF'
+            strokeWidth='8'
+            strokeLinecap='round'
+          />
+          <path
+            d='M74.9027 121.5L143.466 57.5'
+            stroke='#D364ED'
+            strokeWidth='8'
+            strokeLinecap='round'
+          />
+          <path
+            d='M37.6518 122.5L107.835 57.5'
+            stroke='#EA7BEF'
+            strokeWidth='8'
+            strokeLinecap='round'
+          />
+          <path
+            d='M71.1236 59C71.1236 59 14.4374 114 11.7381 116.5C9.03879 119 6.33945 119.5 4.71983 115.5C3.10021 111.5 4.71983 1 4.71983 1'
+            stroke='#FF94FF'
+            strokeOpacity='0.92'
+            strokeWidth='8'
+            strokeLinecap='round'
+          />
+        </svg>
+      </div>
       <h1>Pallidum</h1>
     </div>
   )
