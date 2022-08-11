d637779d02e8554d2c8e998961d1991c3c57cf49
RomainRoy83 <romain.roy.83@gmail.com>
2022-08-11 15:38:33 +0200

    created router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import './assets/index.scss'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Intro from '../components/Intro'
 
 const Home = () => {
   return (
     <div className='Home'>
-      <Intro />
+      <Routes>
+        <Route path='/' element={<Intro />} />
+        {/* <Route path='/info' element={<Info />} />
+        <Route path='/chronologie' element={<Chronology />} />
+        <Route path='/stars' element={<Stars />} />
+        <Route path='/evenements' element={<Event />} />
+        <Route path='/inscription' element={<UserForm />} /> */}
+      </Routes>
     </div>
   )
 }
